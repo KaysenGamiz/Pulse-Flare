54a90f72fb80e47c0dd4db14e1fa5613d2089f18
KaysenGamiz <k.aiseen@hotmail.com>
2025-04-15 12:47:04 -0700

    temperatures enabled

--- controllers/router.js
+++ controllers/router.js
@@ -11,6 +11,7 @@ const analytics_router = require(path.join(__dirname, '..' , 'routes', 'corte_an
 const reports_router = require(path.join(__dirname, '..' , 'routes', 'cloud_reports', 'cloud_reports_route.js'));
 const factura_analyzer_router = require(path.join(__dirname, '..' , 'routes', 'general_utils', 'factura_analyzer', 'factura_analyzer_route.js'));
 const xml_formatter_router = require(path.join(__dirname, '..' , 'routes', 'general_utils', 'xml_formatter', 'xml_formatter_route.js'));
+const readings_router = require(path.join(__dirname, '..', 'routes', 'general_utils', 'temperatures', 'route_reading.js'));
 
 router.use('/hoja_corte', hoja_router);
 router.use('/corte_explorer', explorer_router);
@@ -18,6 +19,7 @@ router.use('/corte_analytics', analytics_router);
 router.use('/cloud_reports', reports_router);
 router.use('/general_utils/factura_analyzer', factura_analyzer_router)
 router.use('/general_utils/xml_formatter', xml_formatter_router)
+router.use('/general_utils/temperatures', readings_router);
 
 router.use('/cortes', corte_router);
 router.use('/employees', employee_router);
